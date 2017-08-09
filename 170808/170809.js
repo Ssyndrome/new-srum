669ecb968355fe516ea0665e04ab68d58b6259e1
徐芽 <915448715@qq.com>
2017-08-09 14:45:26 +0800

    add 170809

---
+++ 170808/170809.js
@@ -0,0 +1,12 @@
+function getN(num){
+    let arr = [0,1];
+
+    if(num === 1){return 0;}
+    if(num === 2){return 1;}
+
+    for(let i = 2;i < num+1;i++){
+        arr.push(arr[i-2]+arr[i-1]);
+    }
+    return arr[num-1];
+}
+console.log(getN(10));
