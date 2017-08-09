f2c03946e845570d774b36ed0f0606b47549742c
915448715@qq.com <915448715@qq.com>
2017-08-09 15:06:35 +0800

    rename

--- 170808~170815/170809.js
+++ 170808~170815/170809.js
@@ -7,6 +7,6 @@ function getN(num){
     for(let i = 2;i < num+1;i++){
         arr.push(arr[i-2]+arr[i-1]);
     }
-    return arr[num-1];
+    return `裴波拉契数列的第${num}个数是${arr[num-1]}`;
 }
 console.log(getN(10));
