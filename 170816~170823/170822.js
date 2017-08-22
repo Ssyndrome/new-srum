5f6e0a90fab9447340bdc57c8d6047398d5d79b2
915448715@qq.com <915448715@qq.com>
2017-08-22 15:55:52 +0800

    correct 170822

--- 170816~170823/170822.js
+++ 170816~170823/170822.js
@@ -1,6 +1,28 @@
 function main(collectionA, collectionB) {
-    let a = new Set(collectionA.filter(A=>{return A}));
-    let b = new Set(collectionB.filter(B=>{return B}));
-    return new Set([...a].filter(x => b.has(x)));
+    let calA = new Map();
+    let calB = new Map();
+    let resObj = new Map();
+
+    collectionA.sort((a, b)=>{return a-b;}).map(A=>{calA.set(A, calA.get(A)?calA.get(A)+1:1);});
+    collectionB.sort((a, b)=>{return a-b;}).map(B=>{calB.set(B, calB.get(B)?calB.get(B)+1:1);});
+
+    [...calA].forEach(A=>{
+        [...calB].forEach(B=>{
+            if(A[0] === B[0]){
+                resObj.set(A[0],A[1]>B[1]?B[1]:A[1]);
+            }
+        });
+    });
+
+    let num = 0;
+    let res_arr = [];
+    [...resObj].map(res=>{
+        num = res[1];
+        while (num){
+            res_arr.push(res[0]);
+            num--;
+        }
+    });
+    return res_arr;
 }
-console.log(main([1,2,2,2,3],[2,3,3,3,4]))
+console.log(main([1,2,2,2,3],[2,2,3,3,3,4]))
